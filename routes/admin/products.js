1a66fbf7fc136877eafb72ea24a8326806ca92cb
Karl <enriquezkharl14@gmail.com>
2021-06-25 20:19:06 +0800

    Multi part forms

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -20,7 +20,10 @@ router.get("/admin/products/new", (req, res) => {
 
 router.post("/admin/products/new", [requireTitle, requirePrice], (req, res) => {
    const errors = validationResult(req);
-   console.log(errors);
+   // console.log(errors);
+   req.on("data", (data) => {
+      console.log(data.toString());
+   });
    res.send("Submited");
 });
 
