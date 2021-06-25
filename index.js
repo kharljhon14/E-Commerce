eb4f5495c596b7246e10652385b968484e10bb31
Karl <enriquezkharl14@gmail.com>
2021-06-25 11:44:49 +0800

    Created a products route

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const { urlencoded } = require("express");
 const express = require("express");
 const cookieSession = require("cookie-session");
 const authRouter = require("./routes/admin/auth");
+const productsRouter = require("./routes/admin/products");
 
 const app = express();
 
 //MiddleWares
 //Check in the current directory and find the "Public" folder and make everything inside the folder available to the outside world
-app.use(express.static("public"))
+app.use(express.static("public"));
 
 app.use(urlencoded({ extended: true }));
 app.use(
@@ -15,8 +16,9 @@ app.use(
       keys: ["asd2323ffs"],
    })
 );
-
+//Middleware Routers
 app.use(authRouter);
+app.use(productsRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
---
+++ routes/admin/products.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+
+//List all Products
+//Create a product
+//submit product
+//edit product
+//edit and sumbit product
+//delete product
+
+router.get("/admin/products", (req, res) => {});
+
+router.get("/admin/products/new", (req, res) => {});
+
+module.exports = router;
