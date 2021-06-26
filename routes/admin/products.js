a08ef340d5e26759186e70e85fa48b39dc9dbc50
Karl <enriquezkharl14@gmail.com>
2021-06-26 09:22:39 +0800

    Products bug fix

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -15,8 +15,12 @@ router.get("/admin/products/new", (req, res) => {
    res.send(productsNewTemplate({}));
 });
 
-router.post("/admin/products/new", [requireTitle, requirePrice], upload.single("image"), async (req, res) => {
+router.post("/admin/products/new", upload.single("image"), [requireTitle, requirePrice], async (req, res) => {
    const errors = validationResult(req);
+
+   if (!errors.isEmpty()) {
+      return res.send(productsNewTemplate({ errors }));
+   }
 
    //Not the best way to store file only use for personal projects
    const image = req.file.buffer.toString("base64");
