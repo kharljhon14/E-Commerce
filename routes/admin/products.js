9bedcedba87ea6dc1e25058a9f1bd535a24028af
karl <enriquezkharl14@gmail.com>
2021-06-27 14:09:46 +0800

    Added product listing

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -5,23 +5,33 @@ const multer = require("multer");
 const { handleError } = require("./middlewares");
 const productsRepo = require("../../repositories/products");
 const productsNewTemplate = require("../../views/admin/products/new");
+const productsIndexTemplate = require("../../views/admin/products/index");
 const { requireTitle, requirePrice } = require("./validators");
 
 const upload = multer({ storage: multer.memoryStorage() });
 
-router.get("/admin/products", (req, res) => {});
+router.get("/admin/products", async (req, res) => {
+  const products = await productsRepo.getAll();
+  res.send(productsIndexTemplate({ products }));
+});
 
 router.get("/admin/products/new", (req, res) => {
-   res.send(productsNewTemplate({}));
+  res.send(productsNewTemplate({}));
 });
 
-router.post("/admin/products/new", upload.single("image"), [requireTitle, requirePrice], handleError(productsNewTemplate), async (req, res) => {
-   //Not the best way to store file only use for personal projects
-   const image = req.file.buffer.toString("base64");
-   const { title, price } = req.body;
+router.post(
+  "/admin/products/new",
+  upload.single("image"),
+  [requireTitle, requirePrice],
+  handleError(productsNewTemplate),
+  async (req, res) => {
+    //Not the best way to store file only use for personal projects
+    const image = req.file.buffer.toString("base64");
+    const { title, price } = req.body;
 
-   await productsRepo.create({ title, price, image });
-   res.send("Submited");
-});
+    await productsRepo.create({ title, price, image });
+    res.send("Submited");
+  }
+);
 
 module.exports = router;
