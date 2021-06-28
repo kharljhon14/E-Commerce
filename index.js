692f084fb5eea8efaf014eef2a069f4b2671c484
karl <enriquezkharl14@gmail.com>
2021-06-28 10:50:31 +0800

    Boilerplaye

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cookieSession = require("cookie-session");
 const authRouter = require("./routes/admin/auth");
 const adminProductsRouter = require("./routes/admin/products");
 const productsRouter = require("./routes/products");
+const cartsRouter = require("./routes/carts");
 
 const app = express();
 
@@ -22,6 +23,7 @@ app.use(
 app.use(authRouter);
 app.use(adminProductsRouter);
 app.use(productsRouter);
+app.use(cartsRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
