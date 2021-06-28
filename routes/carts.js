612e0fb7f80d0f0dca2d981e09e81841239f2d57
karl <enriquezkharl14@gmail.com>
2021-06-28 10:50:22 +0800

    Boilerplate

---
+++ routes/carts.js
@@ -0,0 +1,8 @@
+const express = require("express")
+const router = express.Router();
+
+//Receive a post request to add an item in the cart
+//Receive a get request to show all items in cart
+//receive a post to delete item in the cart
+
+module.exports = router;
